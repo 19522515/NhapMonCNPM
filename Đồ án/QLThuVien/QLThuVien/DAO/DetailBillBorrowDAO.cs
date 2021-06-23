3aa3ace606de4202886d8d9bad9328668ace407a
unknown <19522531@gm.uit.edu.vn>
2021-06-23 13:18:15 +0700

    add

---
+++ Đồ án/QLThuVien/QLThuVien/DAO/DetailBillBorrowDAO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLThuVien.DAO
+{
+    public class DetailBillBorrowDAO
+    {
+        private static DetailBillBorrowDAO instance;
+
+        public static DetailBillBorrowDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DetailBillBorrowDAO();
+                }
+                return instance;
+            }
+            private set => instance = value;
+        }
+
+        public DataTable GetDetailBillBorrowByID(int id)
+        {
+            string query = String.Format("USP_GETDETAILBILLBORROWBYIDBILLBORROW @IDBILLBORROW = {0} ", id);
+            DataTable data = DataProvider.Instance.ExecuteQuery(query).Tables[0];
+            return data;
+
+        }
+        public bool InsertDetailBillBorrow(int IdBook , int idBillBorrow)
+        {
+            string query = string.Format("EXEC USP_INSERTDETAILBILLBORROW @IDBOOK = {0}, @IDBILLBORROW = {1} ", IdBook, idBillBorrow);
+            int data = DataProvider.Instance.ExecutenonQuery(query);
+            return data > 0;
+        }
+      
+    }
+}
---
+++ Đồ án/QLThuVien/QLThuVien/DTO/DetailBillReturn.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLThuVien.DTO
+{
+    public class DetailBillReturn
+    {
+        int idDetailBillReturn;
+        int idBook;
+        int idBillReturn;
+        int idBillBorrow;
+        int fine;
+
+        public DetailBillReturn(int idDetailBillReturn,int idBook,int idBillReturn,int idBillBorrow,  int fine)
+        {
+            this.idDetailBillReturn = idDetailBillReturn;
+            this.idBook = idBook;
+            this.idBillReturn = idBillReturn;
+            this.idBillBorrow = idBillBorrow;
+            this.fine = fine;
+        }
+
+        public DetailBillReturn(DataRow row)
+        {
+            this.idDetailBillReturn = Convert.ToInt32(row["mactphieutra"]);
+            this.idBook = Convert.ToInt32(row["masach"]);
+            this.idBillReturn = Convert.ToInt32(row["maphieutra"]);
+            this.idBillBorrow = Convert.ToInt32(row["maphieumuon"]);
+            this.fine = Convert.ToInt32(row["tienphat"]);
+        }
+
+ 
+        public int IdDetailBillReturn { get => idDetailBillReturn; set => idDetailBillReturn = value; }
+        public int IdBook { get => idBook; set => idBook = value; }
+        public int IdBillReturn { get => idBillReturn; set => idBillReturn = value; }
+        public int IdBillBorrow { get => idBillBorrow; set => idBillBorrow = value; }
+        public int Fine { get => fine; set => fine = value; }
+    }
+}
